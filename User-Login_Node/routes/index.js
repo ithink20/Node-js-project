504794617503fd7be2997ab0a593096a34554d9c
ithink20 <suicidesquade21@gmail.com>
2019-04-17 10:29:07 +0530

    added nodejs files

--- User-Login_Node/routes/index.js
+++ User-Login_Node/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+//home page
 router.get('/', (req, res) => res.render('home'));
 
 module.exports = router;
--- User-Login_Node/routes/users.js
+++ User-Login_Node/routes/users.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const mysql = require('mysql');
 const bcrypt = require('bcryptjs');
 var moment = require('moment');
-
+const crypto = require('crypto');
+global.user_email;
+global.user_name;
 const connection = mysql.createConnection({
     host     : 'db-intern.ciupl0p5utwk.us-east-1.rds.amazonaws.com', //mysql database name
     port     :  '3306',
@@ -23,13 +25,30 @@ connection.connect((err) => {
 router.get('/login', (req, res) => res.render('login'));
 //register page
 router.get('/register', (req, res) => res.render('register'));
+// user-search page
+router.get('/persons', (req, res) => res.render('persons'));
+//dashboard page
+router.get('/dashboard', (req, res) => res.render('dashboard'));
+//update page
+router.get('/update', (req, res) => res.render('update'));
 
-//test
-router.get('/', function (req, res, next) {
+//REST API
+router.get('/', function (req, res) {
     connection.query('SELECT * FROM userData', function (error, results, fields) {
         if (error) throw error;
         res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
     });
+});
+
+router.post('/persons', (req, res) => {
+    var { search } = req.body;
+    connection.query('SELECT userName, emailId, phoneNo FROM userData WHERE emailId = "' + search + '" ', function (error, results, fields) {
+        if (results.length > 0) {
+            res.render('persons', { query: results });
+        } else {
+            res.render('persons', { query: results });
+        }
+    })
 });
 
 //Register Handle
@@ -75,16 +94,16 @@ router.post('/register', (req, res) => {
                 })
             } else {
                 //Hash password
-                bcrypt.genSalt(10, (err, salt) => bcrypt.hash(password, salt, (err, hash) => {
-                    if (err) throw err;
-                    password = hash;
-                    //save user
-                    var time = moment().format();
-                    connection.query('INSERT INTO userData VALUES ("' + name + '","' + email + '","' + phone + '","' + password + '","' + time + '")', function (error, res) {
-                        if (error) throw error;
-                        console.log(name + " is inserted");
-                    });
-                }));
+                let salt = crypto.randomBytes(16).toString('base64');
+                // console.log(req.body.password);
+                let hash = crypto.createHmac('sha256', salt).update(req.body.password).digest("base64");
+                password = salt + "$" + hash;
+                var time = moment().format();
+                connection.query('INSERT INTO userData VALUES ("' + name + '","' + email + '","' + phone + '","' + password + '","' + time + '")', function (error, res) {
+                    if (error) throw error;
+                    console.log(name + " is inserted");
+                });
+
                 req.flash('success_msg', 'you are registered & can login!');
                 res.redirect('/users/login');
             }
@@ -92,21 +111,99 @@ router.post('/register', (req, res) => {
     }
 });
 
+//Update Handle
+router.post('/update', (req, res) => {
+    var { name, phone, password, password2 } = req.body;
+    //check password match
+    var time = moment().format();
+    let errors = [];
+    if (name.length > 0) {
+        connection.query('UPDATE userData SET userName = "' + name + '", dateTime = "' + time + '" WHERE emailID = "' + global.user_email + '"', function (error, results, fields) {
+            if (error) throw error;
+            console.log('name is updated');
+        });
+    }
+    if (phone.length > 0) {
+        connection.query('UPDATE userData SET phoneNo = "' + phone + '", dateTime = "' + time + '" WHERE emailID = "' + global.user_email + '"', function (error, results, fields) {
+            if (error) throw error;
+            console.log('phone is updated');
+        });
+    }
+    if (password.length > 0) {
+        if (password != password2) {
+            errors.push({ msg: 'passwords don\'t match' });
+        }
+        //check pass length
+        if (password.length < 6) {
+            errors.push({ msg: 'password should be atleast 6 characters' });
+        }
+        if (errors.length > 0) {
+            res.render('register', {
+                errors,
+                name,
+                phone,
+                password,
+                password2
+            })
+        }
+        let salt = crypto.randomBytes(16).toString('base64');
+        // console.log(req.body.password);
+        let hash = crypto.createHmac('sha256', salt).update(password).digest("base64");
+        password = salt + "$" + hash;
+        connection.query('UPDATE userData SET password = "' + password + '", dateTime = "' + time +'" WHERE emailID = "' + global.user_email + '"', function (error, results, fields) {
+            if (error) throw error;
+            console.log('password is updated');
+        });
+    }
+    if (name.length > 0) {
+        res.render('dashboard', { query: name });
+    } else {
+        res.render('dashboard', { query: global.user_name });
+    }
+});
+
+//Login Handle
 router.post('/login', (req, res) => {
+    let errors = [];
     var email = req.body.email;
     var password = req.body.password;
+    global.user_email = email;
     connection.query('SELECT * FROM userData WHERE emailId = "'+ email +'" ', function (error, results, fields) {
         if (error) throw error;
+
         //match password
-        // TODO: compare is not working. check it.
-        bcrypt.compare(password, results[0].password, function (err, isMatch) {
-            if (isMatch == true) {
+        if (results.length > 0) {
+            let passwordField = results[0].password.split('$');
+            let salt = passwordField[0];
+            let hash = crypto.createHmac('sha256', salt).update(req.body.password).digest("base64");
+            let tosplit = hash.split(passwordField[1]);
+            let toMatch = hash.split(tosplit[1]);
+            global.user_name = results[0].userName;
+            if (toMatch[0] == passwordField[1]) {
                 console.log('user ' + results[0].userName + ' is matched. Wohooo!');
+                res.render('dashboard', { query: results[0].userName });
+                //TODO:
+
             } else {
-                console.log('user doesn\'t match..');
+                errors.push({ msg: 'Incorrect Password!' });
+                res.render('login', {
+                    errors,
+                    email
+                })
             }
-        });
+        } else {
+            req.flash('error_msg', 'Email does\'nt exist you need to register first!');
+            res.redirect('/users/register');
+        }
     });
 });
+
+// Logout Handle
+router.get('/logout', (req, res) => {
+    req.flash('success_msg', 'You are logged out!');
+    res.redirect('/users/login');
+});
+
+
 
 module.exports = router;
