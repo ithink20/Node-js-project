16aeca9b40148eecbf1791abdab08003b3d1cb1b
vikas chaurasiya <suicidesquade21@gmail.com>
2019-04-18 11:59:05 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+npm install crypto
+npm install mysql
+npm install express
+npm install express-session
+npm install express-validator
+npm install express-ejs-layouts
+npm install connect-flash
+npm i moment
+#etc.
