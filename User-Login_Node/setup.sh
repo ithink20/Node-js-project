44200021ed5e4ce21e3803b889568cfefda05760
ithink20 <suicidesquade21@gmail.com>
2019-04-18 13:54:27 +0530

    updated setup.sh

--- User-Login_Node/setup.sh
+++ User-Login_Node/setup.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
-npm install crypto
-npm install mysql
-npm install express
-npm install express-session
-npm install express-validator
-npm install express-ejs-layouts
-npm install connect-flash
-npm i moment
-#etc.
+npm install
